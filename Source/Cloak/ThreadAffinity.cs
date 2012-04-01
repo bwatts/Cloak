68c246633793ce05206d896001830d0d8098044d
Bryan <Watts>
2012-04-01 01:14:36 -0400

    ThreadAffinity

---
+++ Source/Cloak/ThreadAffinity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Cloak
+{
+	/// <summary>
+	/// An association to a particular thread
+	/// </summary>
+	public sealed class ThreadAffinity
+	{
+		/// <summary>
+		/// Initializes an affinity with the specified thread
+		/// </summary>
+		/// <param name="associatedThread">The associated thread</param>
+		public ThreadAffinity(Thread associatedThread)
+		{
+			Contract.Requires(associatedThread != null);
+
+			AssociatedThread = associatedThread;
+		}
+
+		/// <summary>
+		/// Initializes an affinity with the current thread
+		/// </summary>
+		public ThreadAffinity() : this(Thread.CurrentThread)
+		{}
+
+		/// <summary>
+		/// Gets the associated thread
+		/// </summary>
+		public Thread AssociatedThread { get; private set; }
+
+		/// <summary>
+		/// Determines if the current thread is the associated thread
+		/// </summary>
+		/// <returns>Whether the current thread is the associated thread</returns>
+		public bool Check()
+		{
+			return Thread.CurrentThread == AssociatedThread;
+		}
+
+		/// <summary>
+		/// Throws an exception if the current thread is not the associated thread
+		/// </summary>
+		/// <exception cref="ThreadAffinityException">Thrown if the current thread is not the associated thread</exception>
+		public void Enforce()
+		{
+			if(!Check())
+			{
+				throw new ThreadAffinityException(this);
+			}
+		}
+	}
+}
---
+++ Source/Cloak/ThreadAffinityException.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace Cloak
+{
+	/// <summary>
+	/// Indicates a cross-thread access error
+	/// </summary>
+	public class ThreadAffinityException : Exception
+	{
+		/// <summary>
+		/// Initializes an exception with the specified thread affinity
+		/// </summary>
+		/// <param name="threadAffinity">The thread affinity which caused the error</param>
+		public ThreadAffinityException(ThreadAffinity threadAffinity)
+		{
+			Contract.Requires(threadAffinity != null);
+
+			ThreadAffinity = threadAffinity;
+		}
+
+		/// <summary>
+		/// Initializes an exception with the specified thread affinity
+		/// </summary>
+		/// <param name="threadAffinity">The thread affinity which caused the error</param>
+		/// <param name="message">The exception that describes the error</param>
+		public ThreadAffinityException(ThreadAffinity threadAffinity, string message) : base(message)
+		{
+			Contract.Requires(threadAffinity != null);
+
+			ThreadAffinity = threadAffinity;
+		}
+
+		/// <summary>
+		/// Initializes an exception with the specified thread affinity
+		/// </summary>
+		/// <param name="threadAffinity">The thread affinity which caused the error</param>
+		/// <param name="message">The exception that describes the error</param>
+		/// <param name="inner">The exception that is the cause of the current exception</param>
+		public ThreadAffinityException(ThreadAffinity threadAffinity, string message, Exception inner) : base(message, inner)
+		{
+			Contract.Requires(threadAffinity != null);
+
+			ThreadAffinity = threadAffinity;
+		}
+
+		/// <summary>
+		/// Gets the thread affinity which caused the error
+		/// </summary>
+		public ThreadAffinity ThreadAffinity { get; private set; }
+	}
+}
