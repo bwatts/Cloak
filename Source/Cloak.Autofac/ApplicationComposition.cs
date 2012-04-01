abe9f5ca6b624a10f63611c4f6d960885010d97b
Bryan Watts <bwatts@gmail.com>
2012-04-01 02:44:51 -0400

    Added Autofac integration

---
+++ Source/Cloak.Autofac/ApplicationComposition.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using Autofac;
+using Autofac.Core;
+
+namespace Cloak.Autofac
+{
+	/// <summary>
+	/// The composition of an application composed with an Autofac container
+	/// </summary>
+	public sealed class ApplicationComposition : IDisposable
+	{
+		private readonly IModule _module;
+
+		/// <summary>
+		/// Initializes an application composition with the specified module
+		/// </summary>
+		/// <param name="module">The module which provides the application's composition</param>
+		public ApplicationComposition(IModule module)
+		{
+			Contract.Requires(module != null);
+
+			_module = module;
+		}
+
+		#region IDisposable
+		/// <summary>
+		/// Disposes of <see cref="Container"/>
+		/// </summary>
+		public void Dispose()
+		{
+			if(Container != null)
+			{
+				Container.Dispose();
+
+				Container = null;
+			}
+		}
+		#endregion
+
+		/// <summary>
+		/// Gets the container configured by this application composition
+		/// </summary>
+		public IContainer Container { get; private set; }
+
+		/// <summary>
+		/// Initializes <see cref="Container"/>
+		/// </summary>
+		public void Compose()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterModule(_module);
+
+			Container = builder.Build();
+		}
+	}
+
+	/// <summary>
+	/// The composition of an application composed with an Autofac container
+	/// </summary>
+	/// <typeparam name="TModule">The type of module which configures the container</typeparam>
+	public sealed class ApplicationComposition<TModule> : IDisposable where TModule : IModule, new()
+	{
+		#region IDisposable
+		/// <summary>
+		/// Disposes of <see cref="Container"/>
+		/// </summary>
+		public void Dispose()
+		{
+			if(Container != null)
+			{
+				Container.Dispose();
+
+				Container = null;
+			}
+		}
+		#endregion
+
+		/// <summary>
+		/// Gets the container configured by this application composition
+		/// </summary>
+		public IContainer Container { get; private set; }
+
+		/// <summary>
+		/// Initializes <see cref="Container"/>
+		/// </summary>
+		public void Compose()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterModule<TModule>();
+
+			Container = builder.Build();
+		}
+	}
+}
