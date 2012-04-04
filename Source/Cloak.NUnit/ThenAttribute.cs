be0f56ff3e921dd386e5c353dd69b76ef98fda47
Bryan Watts <bwatts@gmail.com>
2012-04-03 22:28:08 -0400

    Added Behavior and ThenAttribute to Cloak.NUnit

---
+++ Source/Cloak.NUnit/ThenAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Cloak.NUnit
+{
+	/// <summary>
+	/// Indicates an assertion on the result of a tested action
+	/// </summary>
+	public class ThenAttribute : TestAttribute
+	{
+
+	}
+}
