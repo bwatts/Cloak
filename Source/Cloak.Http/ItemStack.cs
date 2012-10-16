67febd61f0bd3e4e3f5e441b7fbc9fb746729192
Bryan Watts <bwatts@gmail.com>
2012-10-15 22:28:44 -0400

    Added item stack

---
+++ Source/Cloak.Http/ItemStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloak.Http
+{
+	public class ItemStack<T>
+	{
+		public ItemStack(T rootItem)
+		{
+			Item = rootItem;
+		}
+
+		private ItemStack(T item, ItemStack<T> parent) : this(item)
+		{
+			Parent = parent;
+		}
+
+		public T Item { get; private set; }
+
+		public ItemStack<T> Parent { get; private set; }
+
+		public virtual ItemStack<T> Append(T item)
+		{
+			return new ItemStack<T>(item, this);
+		}
+
+		public IEnumerable<T> GetAllItemsFromHere()
+		{
+			yield return Item;
+
+			var parent = Parent;
+
+			while(parent != null)
+			{
+				yield return parent.Item;
+
+				parent = parent.Parent;
+			}
+		}
+
+		public IEnumerable<T> GetAllItemsFromRoot()
+		{
+			return GetAllItemsFromHere().Reverse();
+		}
+
+		public override string ToString()
+		{
+			return ToString(" ");
+		}
+
+		public string ToString(string separator)
+		{
+			return ToString(separator, item => item.ToString());
+		}
+
+		public string ToString(Func<T, string> itemTextSelector)
+		{
+			return ToString(" ", itemTextSelector);
+		}
+
+		public string ToString(string separator, Func<T, string> itemTextSelector)
+		{
+			return String.Join(separator, GetAllItemsFromRoot().Select(itemTextSelector));
+		}
+	}
+}
