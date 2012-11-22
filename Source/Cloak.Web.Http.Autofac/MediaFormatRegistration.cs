8ed837f15197d345f48cbaa4a7c24c105597ca81
Bryan Watts <bwatts@gmail.com>
2012-11-22 02:48:31 -0500

    Added registration of media formats

---
+++ Source/Cloak.Web.Http.Autofac/MediaFormatRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Autofac;
+using Cloak.Http.Media;
+using Cloak.Web.Http.Autofac;
+
+namespace Cloak.Web.Http.Autofac
+{
+	public static class MediaFormatRegistration
+	{
+		public static void RegisterMediaFormat<T>(this HttpConfiguration httpSettings, ContainerBuilder builder) where T : MediaFormat, new()
+		{
+			Contract.Requires(httpSettings != null);
+			Contract.Requires(builder != null);
+
+			var format = new T();
+
+			builder.RegisterInstance(format).As<MediaFormat>();
+
+			httpSettings.Formatters.Add(format);
+		}
+	}
+}
---
+++ Source/Cloak.Web.Http.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cloak.Web.Http.Autofac")]
+[assembly: AssemblyDescription("Composition of web API applications using the Autofac engine")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: Guid("D27874E8-7A43-4EC6-8CBE-15751D799DCC")]
