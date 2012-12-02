7b9ec8becead6401666c54ecf38002b2978620d7
Bryan Watts <bwatts@gmail.com>
2012-12-02 00:28:27 -0500

    Renamed ConstantSequence to ExpressionSequence to better indicate its behavior of only converting non-expression instances

---
+++ Source/Cloak/Linq/ExpressionSequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Cloak.Linq
+{
+	/// <summary>
+	/// Extends sequences with the ability to select sequences of <see cref="Expression"/> objects
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public static class ExpressionSequence
+	{
+		/// <summary>
+		/// Gets a sequence of <see cref="Expression"/> objects representing the items in the specified sequence
+		/// </summary>
+		/// <param name="source">
+		/// The sequence from which <see cref="Expression"/> objects are created. Items which are not instances of
+		/// <see cref="Expression"/> are converted to instances of <see cref="ConstantExpression"/>.
+		/// </param>
+		/// <returns>A sequence of <see cref="Expression"/> objects representing the items in the specified sequence</returns>
+		[Pure]
+		public static IEnumerable<Expression> ToExpressions<T>(this IEnumerable<T> source)
+		{
+			Contract.Requires(source != null);
+
+			return source.Select<T, Expression>(item => (item as Expression) ?? Expression.Constant(item));
+		}
+	}
+}
